73f64b82ea27696b3cea0babf83bf6f64ea14c42
CristianVp <cfvargasp.edu@gmail.com>
2025-04-09 21:22:48 +0200

    Update P.py

--- P.py
+++ P.py
@@ -1,1 +1,10 @@
-print("hola")
+Empleado = ["Ana","Luis","Marta"]
+
+for em in Empleado:
+  Pro = int(input(f"Ingrese cantidad de prtoyectos completados por {em}:"))
+  if Pro >=5:
+    bono = Pro*100
+    print(f"El bono de {em} es de {bono}")
+  else:
+    bono = Pro*50
+    print(f"El bono de {em} es de {bono}")
